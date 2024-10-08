212b4b54275b9cd08a0f04760872793781e0d513
Visakh Padmanabhan <visakhpadmanabhan7@gmail.com>
2024-10-08 11:37:42 +0200

    visakh commit3

--- play_tennis.py
+++ play_tennis.py
@@ -3,7 +3,7 @@ from game import Game
 
 nadal = Player("Rafael Nadal")
 djokovic = Player("Novak djokovic")
-shreeya = Player("Shreeya CY")
+shreeya = Player("Shreeya CY123")
 
 test_game = Game((nadal,djokovic),1)
 print(test_game.scores)
